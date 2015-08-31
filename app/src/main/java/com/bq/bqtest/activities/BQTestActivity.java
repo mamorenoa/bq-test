ce6c03f68b04c31f072ac871e120c823f27ff41c
Miguel Ángel Moreno <miguel.angel@mo2o.com>
2015-08-31 22:35:11 +0200

    Load login views in LoginActivity

--- app/src/main/java/com/bq/bqtest/activities/BQTestActivity.java
+++ app/src/main/java/com/bq/bqtest/activities/BQTestActivity.java
@@ -1,8 +1,11 @@
 package com.bq.bqtest.activities;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by miguelangel on 31/8/15.
  */
-public class BQTestActivity
+public class BQTestActivity extends AppCompatActivity
 {
+
 }
--- app/src/main/java/com/bq/bqtest/activities/LoginActivity.java
+++ app/src/main/java/com/bq/bqtest/activities/LoginActivity.java
@@ -4,11 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.bq.bqtest.R;
 
-public class LoginActivity extends AppCompatActivity
+public class LoginActivity extends BQTestActivity
 {
+    private EditText mEtUser;
+    private EditText mEtPassword;
+    private Button mBtContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -16,4 +21,6 @@ public class LoginActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
 }
