6ab8e41ae026cb86acf9c54d62aefa728bf6470a
Miguel Ángel Moreno <miguel.angel@mo2o.com>
2015-08-31 22:29:10 +0200

    first commit

---
+++ app/src/main/java/com/bq/bqtest/application/BQTestApplication.java
@@ -0,0 +1,15 @@
+package com.bq.bqtest.application;
+
+import android.app.Application;
+
+/**
+ * Created by miguelangel on 31/8/15.
+ */
+public class BQTestApplication extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+}
